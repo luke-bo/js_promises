207c8d36d196845c92723d6f0521b4867a531af7
luke.bohonis <w.bohonis@gmail.com>
2020-01-24 11:50:36 -0800

    Add asyncAwait.js. Demonstrates how to use Async Await with delay().

--- lecture/asyncAwait.js
+++ lecture/asyncAwait.js
@@ -1,36 +1,38 @@
-function delay(ms, val) {
-  return new Promise((res, rej) => {
-    if (val === 'black horse') {
-      rej('No black horses')
-    }
-    // setTimeout is the actual asyncrhonous function here.
-    setTimeout(() => {
-      // this callback gets executed when the asynchronous setTimeout has completed.
-      // when it does complete we call the Promise's `resolve()` function
-      res(val)
-    }, ms)
-  })
+// Async Await is new syntax for handling promises
+const delay = require('./delay');
+
+async function doSomething() {
+  try {
+    const blue = await delay(1000, "green");
+    console.log(blue);
+    const brown = await delay(1000, "brown");
+    console.log(brown);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+doSomething();
 
 // Async Await is abstraction on top of promises (like syntax sugar)
 
 // To use Async Await you need Node Version 8+
 // You define a asynchronous function using a new keyword `async`
 
-async function timer() {
+// async function timer() {
   // inside of asynchronous functions we have the `await` keyword available.
   // you can prepend await on any promise and it will resolve before moving onto the next line
-  const a = await delay(1000, 'horse a');
+  // const a = await delay(1000, 'horse a');
   // !Important all code inside of async functions run sequentially
-  const b = await delay(2000, 'horse b');
+  // const b = await delay(2000, 'horse b');
 
   // if you do want to run asynchronous code in parallel you need to wrap them in Promise.all
-  const c = await Promise.all([ //promises here run in parallel :)
-    delay(2000, 'horse c'),
-    delay(2000, 'horse d')
-  ]);
+  // const c = await Promise.all([ //promises here run in parallel :)
+    // delay(2000, 'horse c'),
+    // delay(2000, 'horse d')
+  // ]);
 
-  console.log(a);
-}
+  // console.log(a);
+// }
 
-timer();
+// timer();
